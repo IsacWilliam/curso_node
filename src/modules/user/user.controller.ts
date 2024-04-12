4c80815564ed5541a3f7110ff275a206457cb1d8
Isac William <hailisac@hotmail.com>
2024-04-12 18:19:36 -0300

    Inserindo Exception em service e controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,21 @@
 import { Request, Response, Router } from "express";
 import { createUser, getUsers } from "./user.service";
 import { UserInsertDTO } from "./dtos/user-insert.dto";
+import { NotFoundException } from "@exceptions/not-found-exception";
 
 const userRouter = Router();
 
 userRouter.use('/user', userRouter);
 
 userRouter.get('/', async (_, res: Response): Promise<void> =>{
-    const users = await getUsers();
+    const users = await getUsers().catch((error) => {
+        if(error instanceof NotFoundException){
+            res.status(204);
+        }else{
+            res.status(500).send(error.message);
+        }
+    });
+
     res.json(users);
 });
 
