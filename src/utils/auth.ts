abca480eac0d8d83ce955286751363b0b143101b
Isac William <hailisac@hotmail.com>
2024-04-16 10:58:48 -0300

    Criação de Token JWT

---
+++ src/utils/auth.ts
@@ -0,0 +1,20 @@
+import { UserModel } from "@modules/user/user.model";
+import { sign } from "jsonwebtoken";
+
+export const PASSWORD_JWT = 'senha';
+
+export const generateToken = (user: UserModel): string => {
+
+    return sign(
+        {
+            userId: user.id,
+            email: user.email,
+            typeUser: user.typeUser,
+        },
+         PASSWORD_JWT,
+        {
+            subject: String(user.id),
+            expiresIn: '604800000',
+        }
+    );
+}
